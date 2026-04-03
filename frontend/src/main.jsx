45969a7dddb703692372e278a958516fbe8e9b46
Victor Osaikhuwuomwan <osasv35@gmail.com>
2026-04-03 10:44:28 -0400

    Connect App component to main file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function App() {
-  return (
-    <div>
-      <h1>Smart Home Temperature Monitor</h1>
-      <p>Frontend setup in progress.</p>
-    </div>
-  );
-}
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
